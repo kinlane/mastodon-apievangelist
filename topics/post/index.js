2ee486316ee1c3ed96352d74a9eb2c603c41e313
Kin Lane <kinlane@gmail.com>
2023-01-07 15:58:29 -0800

    add topics

--- topics/post/index.js
+++ topics/post/index.js
@@ -10,10 +10,7 @@ exports.handler = vandium.generic()
     password : process.env.password,
     database : process.env.database
     });
-    
-    var sql1 = "SELECT user_id FROM users WHERE fingered = 1 and followed = '0000-00-00' ORDER BY RAND() LIMIT 1;";
-    connection.query(sql1, function (error, result, fields) {   
-      callback( null, result[0] );
-      });    
+
+      callback( null, "hello" );
 
 });
--- users/follow/post/index.js
+++ users/follow/post/index.js
@@ -11,16 +11,14 @@ exports.handler = vandium.generic()
     database : process.env.database
     });
     
+    var today = new Date();
+    var dd = String(today.getDate()).padStart(2, '0');
+    var mm = String(today.getMonth() + 1).padStart(2, '0'); //January is 0!
+    var yyyy = today.getFullYear();
     
-    var description = event.user.note;
-    description = description.replace(/[^a-zA-Z0-9 ]/, '');
-    description = description.replace(/'/g, 'A');
-    description = description.replace(/<[^>]*>?/gm, '');
-    description = description.replace(/(\r\n|\r|\n)/g, '<br>');
-    description = description.replace(/[^\w\s]/gi, '')
-    description = description.replace(/([\u2700-\u27BF]|[\uE000-\uF8FF]|\uD83C[\uDC00-\uDFFF]|\uD83D[\uDC00-\uDFFF]|[\u2011-\u26FF]|\uD83E[\uDD10-\uDDFF])/g, '');
-
-    var sql1 = "UPDATE users SET user_id=" + connection.escape(event.user.id) + ", account=" + connection.escape(event.user.account) + ",name=" + connection.escape(event.user.display_name) + ",description=" + connection.escape(description) + ",url=" + connection.escape(event.user.url) + ",avatar=" + connection.escape(event.user.avatar) + ",followers_count=" + connection.escape(event.user.followers_count) + ",following_count=" + connection.escape(event.user.following_count) + ",statuses_count=" + connection.escape(event.user.statuses_count) + ",followed=1,profiled=1 WHERE account = " + connection.escape(event.user.account);
+    today = yyyy + '-' + mm + '-' + dd;    
+    
+    var sql1 = "UPDATE users SET followed=" + connection.escape(today) + " WHERE user_id = " + connection.escape(event.user_id);
     connection.query(sql1, function (error, result, fields) {   
 
       callback( null, result );
