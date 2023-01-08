963d2c4e4b6f55728706d9831ae6c61170c5ab43
Kin Lane <kinlane@gmail.com>
2023-01-07 16:54:01 -0800

    update

---
+++ topics/pull/post/index.js
@@ -0,0 +1,16 @@
+const vandium = require('vandium');
+const mysql  = require('mysql');
+
+exports.handler = vandium.generic()
+  .handler( (event, context, callback) => {
+
+    var connection = mysql.createConnection({
+    host     : process.env.host,
+    user     : process.env.user,
+    password : process.env.password,
+    database : process.env.database
+    });
+
+      callback( null, "hello" );
+
+});
